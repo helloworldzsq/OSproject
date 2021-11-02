e919291434422c0b24893cb404c4888a8fc0e645
dzsq <2748255621@qq.com>
2021-11-02 20:26:18 +0800

    11-2

--- 实验1/shell1.c
+++ 实验1/shell1.c
@@ -1,49 +1,60 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<errno.h>
 
-//去除首部空格
-void trim(char * cmd1,char * tem){
+//去除首尾部空格
+int trim(char * cmd1,char * tem,char * cmd){
     int m=0;
     while (cmd1[m]==' ')
         m++;
     strncpy(tem,cmd1+m,strlen(cmd1)-m);
     tem[strlen(cmd1)-m] = '\0';
+    //找到第一个出现空格的位置
+    int n=strcspn(tem ," ");
+    if(n>0){
+        strncpy(cmd,tem,n);
+        cmd[n]='\0';
+    }
+    return n;
 }
 int main(){
     char cmd1[80];
-    char str2[80];
     char tem[80];
     char str3[80]={0};
-    char *cmd;
+    char cmd[80];
     char *scwt[] = {"dir","cop" ,"era" ,"disp" ,"end"};
     char *scwt2[] = { "dir","cp", "rm" ,"echo","end" };
     static int cmdnum = 5;
     int j;
+    //子进程的返回数值
+    int rtn;
     while(1){
         printf( "Please input command:");
         gets(cmd1);
-        trim(cmd1,tem);
-        //找到第一个出现空格的位置
-        int n=strcspn(tem ," ");
-        if(n>0 || strlen(tem)>0){
-            strncpy(str2,tem,n);
-            str2[n]='\0';
-            cmd=str2;
+        int n = trim(cmd1,tem,cmd);
+        if(n>0){
             for (j = 0; j < cmdnum; ++j) {
                 if (strcmp(cmd,scwt[j])==0){
                     break;
                 }
             }
-            if (j==4)
+            //内部命令
+            if (j==4){
                 exit(0);
-            if (j<cmdnum){
+            }
+            else if (j<cmdnum){
                 //拼接字符串
                 sprintf(str3,"%s%s",scwt2[j],tem+n);
                 system(str3);
                 continue;
+            } else{
+                if (fork()==0){
+                    execl("/bin/sh","sh","-c", cmd, (char*)0);
+                    perror(cmd);
+                }
+                wait(0);
             }
-            printf("Bad command\n");
         }
     }
     return 0;
---
+++ 实验1/system.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <unistd.h>
+
+int system(const char * cmdstring)
+{
+    pid_t pid;
+    int status;
+    if(cmdstring == NULL){
+        return (1);
+    }
+    if((pid = fork())<0){
+
+        status = -1;
+    }
+    else if(pid == 0){
+        execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);
+        exit(127); //子进程正常执行则不会执行此语句
+    }
+    else{
+        while(waitpid(pid, &status, 0) < 0){
+            if(errno != EINTR){
+                status = -1;
+                break;
+            }
+        }
+    }
+    return status;
+}
